e184ba40a72d12f8cd8f55e6e2a18c623cbfb206
gabs <92656197+xoxostardust@users.noreply.github.com>
2023-05-14 22:03:03 -0700

    tablet query

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 // desktop scroll function
 function show() {
-    let code = document.querySelectorAll(".code")
+  let code = document.querySelectorAll(".code")
 
-    for (let i = 0; i < code.length; i++) {
-        let windowHeight = window.innerHeight;
-        let elementTop = code[i].getBoundingClientRect().top;
-        let elementVisible = 300;
-        if (elementTop < windowHeight - elementVisible) {
-            code[i].classList.add("active");
-          } else {
-            code[i].classList.remove("active");
-          }
-        }
-      }
+  for (let i = 0; i < code.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = code[i].getBoundingClientRect().top;
+    let elementVisible = 300;
+    if (elementTop < windowHeight - elementVisible) {
+      code[i].classList.add("active");
+    } else {
+      code[i].classList.remove("active");
+    }
+  }
+}
 
-      window.addEventListener("scroll", show);
+window.addEventListener("scroll", show);
 
 // mobile scroll function
-      function show2() {
-        let code = document.querySelectorAll(".code")
-    
-        for (let i = 0; i < code.length; i++) {
-            let windowHeight = window.innerHeight;
-            let elementTop = code[i].getBoundingClientRect().top;
-            let elementVisible = 100;
-            if (elementTop < windowHeight - elementVisible) {
-                code[i].classList.add("active");
-              } else {
-                code[i].classList.remove("active");
-              }
-            }
-          }
-    
-          window.addEventListener("scroll", show);
-      
+function show2() {
+  let code = document.querySelectorAll(".code")
+
+  for (let i = 0; i < code.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = code[i].getBoundingClientRect().top;
+    let elementVisible = 100;
+    if (elementTop < windowHeight - elementVisible) {
+      code[i].classList.add("active");
+    } else {
+      code[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", show);
+
 // scrolling check ; determine if mobile or desktop to change conditions 4 show animation
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        // mobile device is true
-        console.log("mobile device");
-        show2();
-      }else{
-        // desktop
-        console.log("not mobile device");
-        show();
-      }
-    
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  // mobile device is true
+  console.log("mobile device");
+  show2();
+} else {
+  // desktop
+  console.log("not mobile device");
+  show();
+}
+
 
 
 
@@ -78,7 +78,7 @@ nextSlide.addEventListener("click", function () {
 
 let curSlide = 0;
 
-let maxSlide = slides.length -1;
+let maxSlide = slides.length - 1;
 
 const prevSlide = document.querySelector("#prev");
 
